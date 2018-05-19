d715ee474bd58dcfa86326529cb38c6f4c7656f2
ELVESZYM <2470884702@qq.com>
2018-05-19 14:15:14 +0800

    老师假如延时，可以返回idel但是还是只能运行一次

--- firmware_mpg_common/application/user_app1.c
+++ firmware_mpg_common/application/user_app1.c
@@ -72,6 +72,13 @@ static u8 UserApp1_au8MessageFail[] = "\n\r***ANT channel setup failed***\n\n\r"
 /**********************************************************************************************************************
 Function Definitions
 **********************************************************************************************************************/
+  static u8 u8select = 0;
+  static u8 u8UserInput[8]; //16点汉字 “啊”  的GB2312内码。 
+  static u8 u8_dot[3][32];			  //存储汉字 “啊”  的点阵数据。 
+
+  u8 u8_counter_,u8showcount=0;
+
+
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Public functions                                                                                                   */
@@ -81,6 +88,330 @@ Function Definitions
 /* Protected functions                                                                                                */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+  /*
+static void Delay(u8 u8Time)
+ {
+   static u8 u8TimeCount=0;
+   u8TimeCount++;
+   if(u8TimeCount == u8Time )
+   {
+     u8TimeCount=0;
+     return;
+   }
+ }
+*/
+static void SendCharMSB(u8 u8Data_)
+{
+ 
+ //  CLR_LED_LAT();
+  
+  u8 u8flag;						//数据标志位
+  u32 u8length;					//数据长度
+  u8 u8test_temp = 0;
+  u8test_temp = u8Data_;
+  for(u8length = 0; u8length < 8; u8length++)	      //发送一个8位数据
+  {
+
+    CLR_LED_CLK(); //时钟线拉低
+    u8flag = u8test_temp & 0x80;	//判断数据最高位状态
+    if(u8flag == 0)							
+    {
+      CLR_LED_DRI();									//当最高位为0时，将数据输出口拉低
+    }
+    else
+    {
+      SET_LED_DRI();									//当最高位为0时，将数据输出口拉低
+    }
+
+    SET_LED_CLK();				//时钟线拉高
+    u8test_temp <<= 1;			//将数据左移1位
+  
+  }
+//   SET_LED_LAT();
+  
+}
+
+//水平显示
+static void LEDDispMoveHorizontal(void)
+{
+  static u8 u8Data;
+  static u8 u8y=0;
+  static u8 u8select=0;
+  static u16 u16Counter=0;
+  u16Counter++;
+  
+  CLR_LED_LAT();
+  
+  for(u8 u8i=0;u8i<15;u8i=u8i+2)
+  {
+
+    SendCharMSB(0x00);
+    SendCharMSB(0x00);
+    for(u8 u8j=0;u8j<3;u8j++)
+    {  
+      SendCharMSB(u8_dot[u8j][u8i+16]);      
+      SendCharMSB(u8_dot[u8j][u8i]);
+      SendCharMSB(u8_dot[u8j][u8i+17]);
+      SendCharMSB(u8_dot[u8j][u8i+1]); 
+    }
+    
+    
+    switch(u8i/2)		//1--8行的输入情况选择
+    {  
+      case 0:   { PA2_CODR();                 
+                  PA1_CODR();
+                  PA0_CODR();
+                 }break;
+      case 1:   { PA2_CODR();
+                  PA1_CODR();  
+                  PA0_SODR();
+                 }break;
+      case 2:   { PA2_CODR();
+                  PA1_SODR();
+                  PA0_CODR();
+                 }break;
+      case 3:	{ PA2_CODR();
+                  PA1_SODR();
+                  PA0_SODR();
+                 }break;
+      case 4:   { PA2_SODR();
+                  PA1_CODR();
+                  PA0_CODR();
+                 }break;
+      case 5:   { PA2_SODR();
+                  PA1_CODR(); 
+                  PA0_SODR();
+                 }break;
+      case 6:   { PA2_SODR();
+                  PA1_SODR();
+                  PA0_CODR();
+                 }break;
+      case 7:   { PA2_SODR();
+                  PA1_SODR();
+                  PA0_SODR(); 
+                 }break;
+    default: break;
+    }
+   // CLR_LED_LAT();
+    SET_LED_LAT();
+    SET_LED_CS1();   
+    for(u8 u8k=0;u8k<200;u8k++);
+    for(u8 u8k=0;u8k<200;u8k++);
+    CLR_LED_CS1();
+    
+  }
+  
+  if(u16Counter == 1000)
+  {
+    u16Counter=0;
+    UserApp1_StateMachine=UserApp1SM_Idle;
+    CLR_LED_LAT();
+
+  }
+
+
+}
+ 
+
+
+
+/**********************************************************************************************************************
+State Machine Function Definitions
+**********************************************************************************************************************/
+
+
+static u8 readbyte(u32 u32addr)  //read one byte from GT ROM
+ { 
+  u8 u8i,u8RT_data=0x03;//赵：RT是Instruction-read方式 
+  u8 u8Printf_screen=0;
+  static u8 u8Instruction_flag;
+  static u32 u32Adress_flag;
+  static u32 u32IOLevel;
+  static u32 u32Doorplate_addr;
+  
+  
+  //CMOSI_PA14();                          //赵：??? 
+
+  for(u8i=0;u8i<8;u8i++)	  //先写入0x03；//赵：发送1个字节的命令-read（03）-fast read（0B） 
+  {
+    u8Instruction_flag = u8RT_data & 0x80;
+    
+    for(u8 u8q=0;u8q<100;u8q++);
+     
+    CSCK_PA15();  
+  
+    if(u8Instruction_flag == 0)
+    {
+      CMOSI_PA14();
+    }
+    else
+    {
+      SMOSI_PA14();
+    }
+    for(u8 u8q=0;u8q<100;u8q++);
+   
+    SSCK_PA15();
+    u8RT_data<<=1;    
+
+  }
+  
+  u32Doorplate_addr=u32addr;
+  
+  for(u8i=0;u8i<24;u8i++)	   //再写三个字节地址 
+  {
+    
+    u32Adress_flag = u32Doorplate_addr & 0x800000;
+    
+    for(u8 u8q=0;u8q<100;u8q++);
+     
+    CSCK_PA15();
+     
+    if(u32Adress_flag == 0)
+    {
+      CMOSI_PA14();
+    }
+    else
+    {
+      SMOSI_PA14();
+    }
+    for(u8 u8q=0;u8q<100;u8q++);
+    
+    SSCK_PA15();
+    
+    u32Doorplate_addr<<=1;
+    
+    
+  }
+ 
+   u8Printf_screen=0;
+  
+  for(u8i=0;u8i<8;u8i++)//读一个字节 
+  {
+    u32IOLevel = IO_STATE;
+    
+    SSCK_PA15();
+      
+    if((u32IOLevel & 0x2000) != 0)
+    {
+     u8Printf_screen|=0x01;
+    }
+
+    CSCK_PA15();
+   
+    u8Printf_screen<<=1;  
+
+  }
+   
+  return (u8Printf_screen);
+
+  
+
+ }
+
+//**************汉字在芯片中的点阵数据起始地址  ****************************
+static u32  GB2312_Addr(u8 *u8internal_code,u8 u8type)
+{
+  static u32 u32temp = 0;
+  static u8 u8MSB,u8LSB;
+  
+  if(u8internal_code[0]<0x80)	   	//ascii  半角字符 
+  {
+    if( u8internal_code[0] >= ' ' )  //判断是否大于“空格键 ” 0x20 
+    {
+       u32temp = u8internal_code[0] - ' ';
+    }
+    if( u8type == TYPE_8 )  	  
+    {
+       u32temp = u32temp*8  + ASC0808D2HZ_ADDR;//7*8 ascii code
+    }  		 	
+    else if( u8type == TYPE_12 )	
+    {
+      u32temp = u32temp*12 + ASC0812M2ZF_ADDR;	//6*12 ascii code
+    }		
+    else if( u8type == TYPE_16 )	
+    {
+      u32temp = u32temp*16 + GBEX0816ZF_ADDR;			//8*16 ascii code
+    }
+  }
+  else               //chinese tab zone
+  {
+    switch (u8showcount)
+    {
+    case 0: u8MSB=u8internal_code[0];
+             u8LSB=u8internal_code[1];
+             break;
+    case 1: u8MSB=u8internal_code[2];
+             u8LSB=u8internal_code[3];
+             break;
+    case 2: u8MSB=u8internal_code[4];
+             u8LSB=u8internal_code[5];
+             break;
+    }
+   
+    //符号区,全角 
+    if(u8MSB>=0xA1 && u8MSB <= 0xAB && u8LSB>=0xa1)
+    {
+      if( u8type == TYPE_12 ) //16 点汉字字符 
+      {
+        if( u8MSB>=0xA1 && u8MSB <= 0xA3 && u8LSB>=0xa1 )//ZF 272 ZONE
+        {
+           u32temp = (u8MSB - 0xA1) * 94 + (u8LSB - 0xA1);
+        }
+        else if( u8MSB == 0xa9 && u8LSB>=0xa1  )//tab 94 zone
+        {
+           u32temp = 282 + ( u8LSB - 0xA1 );
+        }
+         return( u32temp *24 + ZF1112B2ZF_ADDR);		 //11*12 12点字符 
+      }
+      else
+      {
+        u32temp = (u8MSB - 0xA1) * 94 + (u8LSB - 0xA1);
+        return(u32temp *32 + CUTS1516ZF_ADDR);			 //16*16 16点字符 
+      }
+
+    }
+    //GBK双字节2区 6768
+    else if(u8MSB>=0xb0 && u8MSB <= 0xf7 && u8LSB>=0xa1)
+    {
+      u32temp= (u8MSB - 0xB0) * 94 + (u8LSB - 0xA1);
+      if( u8type == TYPE_12 )
+      {
+         u32temp = u32temp*24 + HZ1112B2HZ_ADDR;
+      }
+      else if( u8type == TYPE_16 )
+      {
+         u32temp = u32temp*32 + JFLS1516HZ_ADDR;
+      }
+     
+    }
+  }
+ 
+return u32temp;
+
+}
+/*-------------------------------------------------------------------------------------------------------------------*/
+static void font_character(void)
+{
+  static u32 u32_dot_address;
+
+ u32_dot_address = GB2312_Addr(u8UserInput,TYPE_16); //得到“啊” 点阵数据的起始地址 
+
+  for( u8_counter_ = 0; u8_counter_ < 32; u8_counter_++ )   //得到“啊” 点阵数据 
+  {
+    CCS_PA16();
+    u8_dot[u8showcount][u8_counter_] = readbyte(u32_dot_address+u8_counter_);
+    SCS_PA16();
+  }
+  u8showcount++;
+  if(u8showcount == 3)
+  {
+    u8showcount=0;
+    UserApp1_StateMachine=LEDDispMoveHorizontal;
+
+  }
+}
+
+  
 /*--------------------------------------------------------------------------------------------------------------------
 Function: UserApp1Initialize
 
@@ -97,6 +428,43 @@ void UserApp1Initialize(void)
 {
   u8 au8WelcomeMessage[] = "ANT Master";
 
+ /****************************点阵*********************************/  
+ //初始化PA0-PA6口，设为输出口
+ // AT91C_BASE_PIOA->PIO_PER    |= 0x7F; //点阵口配置
+ // AT91C_BASE_PIOA->PIO_PDR    &= 0xFFFFFF80;//点阵口配置
+  
+  AT91C_BASE_PIOA->PIO_PER    |= 0x1E07F;//点阵口配置和字库io口配置
+  AT91C_BASE_PIOA->PIO_PDR    &= 0xFFFE1F80;//点阵口配置和字库io口配置
+  
+  AT91C_BASE_PIOA->PIO_OER    |= 0x1C07F;
+  AT91C_BASE_PIOA->PIO_ODR    &= 0xFFFE3F80;
+  
+  AT91C_BASE_PIOA->PIO_IFER   |= 0x2000;
+  AT91C_BASE_PIOA->PIO_IFDR   &= 0xFFFFDFFF;
+  
+  AT91C_BASE_PIOA->PIO_SODR   = PIOA_SODR_INIT;
+  AT91C_BASE_PIOA->PIO_CODR   = PIOA_CODR_INIT;
+  
+  AT91C_BASE_PIOA->PIO_MDER   = PIOA_MDER_INIT;
+  AT91C_BASE_PIOA->PIO_MDDR   = PIOA_MDDR_INIT;
+  
+  AT91C_BASE_PIOA->PIO_PPUDR  = PIOA_PPUDR_INIT;
+  AT91C_BASE_PIOA->PIO_PPUER  = PIOA_PPUER_INIT;
+  
+  AT91C_BASE_PIOA->PIO_ABSR   = PIOA_ABSR_INIT;
+  
+  AT91C_BASE_PIOA->PIO_SCIFSR = PIOA_SCIFSR_INIT;
+  AT91C_BASE_PIOA->PIO_DIFSR  = PIOA_DIFSR_INIT;
+  
+  AT91C_BASE_PIOA->PIO_SCDR   = PIOA_SCDR_INIT;
+  
+  AT91C_BASE_PIOA->PIO_OWER   |= 0x1C07F;
+  AT91C_BASE_PIOA->PIO_OWDR   &= 0xFFFE3F80;
+    
+  /*************************************************************/  
+ 
+  
+  
   /* Write a weclome message on the LCD */
 #if EIE1
   /* Set a message up on the LCD. Delay is required to let the clear command send. */
@@ -200,33 +568,11 @@ static void UserApp1SM_Idle(void)
 {
   static u8 au8TestMessage[] = {0, 0, 0, 0, 0xA5, 0, 0, 0};
   u8 au8DataContent[] = "xxxxxxxxxxxxxxxx";
+  static u8 u8AntMessage[]={0xff,0x00,0x01,0x03,0xff,0x00,0x00,0x00};
+  static u8 u8AntMessage_counter=0;
   
-  /* Check all the buttons and update au8TestMessage according to the button state */ 
-  au8TestMessage[0] = 0x00;
-  if( IsButtonPressed(BUTTON0) )
-  {
-    au8TestMessage[0] = 0xff;
-  }
-  
-  au8TestMessage[1] = 0x00;
-  if( IsButtonPressed(BUTTON1) )
-  {
-    au8TestMessage[1] = 0xff;
-  }
-
-#ifdef EIE1
-  au8TestMessage[2] = 0x00;
-  if( IsButtonPressed(BUTTON2) )
-  {
-    au8TestMessage[2] = 0xff;
-  }
-
-  au8TestMessage[3] = 0x00;
-  if( IsButtonPressed(BUTTON3) )
-  {
-    au8TestMessage[3] = 0xff;
-  }
-#endif /* EIE1 */
+ 
+ 
   
   if( AntReadAppMessageBuffer() )
   {
@@ -238,10 +584,14 @@ static void UserApp1SM_Idle(void)
       {
         au8DataContent[2 * i]     = HexToASCIICharUpper(G_au8AntApiCurrentMessageBytes[i] / 16);
         au8DataContent[2 * i + 1] = HexToASCIICharUpper(G_au8AntApiCurrentMessageBytes[i] % 16);
+        u8UserInput[i]=G_au8AntApiCurrentMessageBytes[i];
       }
-
+      UserApp1_StateMachine = font_character;
+      
 #ifdef EIE1
-      LCDMessage(LINE2_START_ADDR, au8DataContent);
+     LCDMessage(LINE2_START_ADDR, au8DataContent);
+     
+      
 #endif /* EIE1 */
       
 #ifdef MPG2
@@ -263,7 +613,21 @@ static void UserApp1SM_Idle(void)
       AntQueueBroadcastMessage(ANT_CHANNEL_USERAPP, au8TestMessage);
     }
   } /* end AntReadData() */
-  
+/*
+   for(u8 u8i=0;u8i<8;u8i++)
+  {
+    if(G_au8AntApiCurrentMessageBytes[u8i] == u8AntMessage[u8i])
+    {
+      u8AntMessage_counter++;
+      if(u8AntMessage_counter == 8)
+      {
+        u8AntMessage_counter=0;
+        UserApp1_StateMachine = font_character;
+      }
+
+    }
+  }
+*/
 } /* end UserApp1SM_Idle() */
 
 
--- firmware_mpg_common/application/user_app1.h
+++ firmware_mpg_common/application/user_app1.h
@@ -50,6 +50,131 @@ Function Declarations
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 
+
+#define G_u8LedDisplayScreenCharUnits  (u8)14 
+
+#define P_29_A4_SCS1 0x00000008
+#define P_29_A4_CCS1 0x00000008
+#define P_30_A6_SDAO 0x00000010
+#define P_30_A6_CDAO 0x00000010
+#define P_31_A7_SLAT 0x00000020
+#define P_31_A7_CLAT 0x00000020
+#define P_32_A8_SCLK 0x00000040
+#define P_32_A8_CCLK 0x00000040
+
+#define SET_LED_CS1()   ( AT91C_BASE_PIOA->PIO_SODR |= 0x08)//CS1
+#define CLR_LED_CS1()   ( AT91C_BASE_PIOA->PIO_CODR |= 0x08)
+#define SET_LED_DRI()   ( AT91C_BASE_PIOA->PIO_SODR |= 0x10)//DAO
+#define CLR_LED_DRI()   ( AT91C_BASE_PIOA->PIO_CODR |= 0x10)
+#define SET_LED_LAT()   ( AT91C_BASE_PIOA->PIO_SODR |= 0x20)//LAT
+#define CLR_LED_LAT()   ( AT91C_BASE_PIOA->PIO_CODR |= 0x20)
+#define SET_LED_CLK()   ( AT91C_BASE_PIOA->PIO_SODR |= 0x40)//CLK
+#define CLR_LED_CLK()   ( AT91C_BASE_PIOA->PIO_CODR |= 0x40)
+
+
+#define PA0_SODR()      ( AT91C_BASE_PIOA->PIO_SODR |= 0x01)
+#define PA0_CODR()      ( AT91C_BASE_PIOA->PIO_CODR |= 0x01)
+#define PA1_SODR()      ( AT91C_BASE_PIOA->PIO_SODR |= 0x02)
+#define PA1_CODR()      ( AT91C_BASE_PIOA->PIO_CODR |= 0x02)
+#define PA2_SODR()      ( AT91C_BASE_PIOA->PIO_SODR |= 0x04)
+#define PA2_CODR()      ( AT91C_BASE_PIOA->PIO_CODR |= 0x04)
+
+#define SCAN_LINE_MODE                		(u8)8 
+
+#define P1_SIGN_DATA_LINES_OFFSET  0
+
+#define SMISO_PA13() (AT91C_BASE_PIOA->PIO_SODR |= 0x2000)
+#define SMOSI_PA14() (AT91C_BASE_PIOA->PIO_SODR |= 0x4000)
+#define SSCK_PA15()  (AT91C_BASE_PIOA->PIO_SODR |= 0x8000)
+#define SCS_PA16()   (AT91C_BASE_PIOA->PIO_SODR |= 0x10000)
+
+#define CMISO_PA13() (AT91C_BASE_PIOA->PIO_CODR |= 0x2000)
+#define CMOSI_PA14() (AT91C_BASE_PIOA->PIO_CODR |= 0x4000)
+#define CSCK_PA15()  (AT91C_BASE_PIOA->PIO_CODR |= 0x8000)
+#define CCS_PA16()   (AT91C_BASE_PIOA->PIO_CODR |= 0x10000)
+
+ 
+//#define IO_STATE     (AT91C_BASE_PIOA->PIO_PDSR &=0xffffffff)
+
+////////////////////////////////////
+
+#define IO_STATE     (AT91C_BASE_PIOA->PIO_PDSR)
+
+////////////////////////////////////
+
+/**********************************************************************************************************************
+Constants / Definitions
+**********************************************************************************************************************/
+#define ASC0808D2HZ_ADDR ( 0x66c0  ) 		//7*8 ascii code
+#define ASC0812M2ZF_ADDR ( 0x66d40 )   		//6*12 ascii code
+#define GBEX0816ZF_ADDR   243648   	   		//8*16 ascii code
+#define ZF1112B2ZF_ADDR  ( 0x3cf80 )	   	//12*12 12µã×Ö·û 
+#define HZ1112B2HZ_ADDR  ( 0x3cf80+376*24 )	//12*12 12µãºº×Ö 
+#define CUTS1516ZF_ADDR  0x00  				//16*16 16µã×Ö·û 
+#define JFLS1516HZ_ADDR  27072  			//16*16 16µãºº×Ö 
+
+//---------------------------------------------
+// ±£Áô 
+//--------------------------------------------
+#define ASCII0507ZF_ADDR        245696
+#define ARIAL_16B0_ADDR         246464
+#define ARIAL_12B0_ADDR         422720
+#define SPAC1616_ADDR           425264
+#define GB2311ToUnicode_addr    (12032)
+#define UnicodeToGB2311_addr    (425328)
+//------------------------------------------------
+// ÒªÏÔÊ¾µÄºº×ÖµÄÀàÐÍ 
+//------------------------------------------------
+#define   TYPE_8  	0 //8  µã×Ö·û 
+#define   TYPE_12  	1 //12 µãºº×Ö×Ö·û 
+#define   TYPE_16  	2 //16 µãºº×Ö×Ö·û 
+
+/**********************************************************************************************************************
+Function Declarations
+**********************************************************************************************************************/
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Public functions                                                                                                   */
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+#define G_u8LedDisplayScreenCharUnits  (u8)14 
+
+#define P_29_A4_SCS1 0x00000008
+#define P_29_A4_CCS1 0x00000008
+#define P_30_A6_SDAO 0x00000010
+#define P_30_A6_CDAO 0x00000010
+#define P_31_A7_SLAT 0x00000020
+#define P_31_A7_CLAT 0x00000020
+#define P_32_A8_SCLK 0x00000040
+#define P_32_A8_CCLK 0x00000040
+
+#define SET_LED_CS1()   ( AT91C_BASE_PIOA->PIO_SODR |= 0x08)//CS1
+#define CLR_LED_CS1()   ( AT91C_BASE_PIOA->PIO_CODR |= 0x08)
+#define SET_LED_DRI()   ( AT91C_BASE_PIOA->PIO_SODR |= 0x10)//DAO
+#define CLR_LED_DRI()   ( AT91C_BASE_PIOA->PIO_CODR |= 0x10)
+#define SET_LED_LAT()   ( AT91C_BASE_PIOA->PIO_SODR |= 0x20)//LAT
+#define CLR_LED_LAT()   ( AT91C_BASE_PIOA->PIO_CODR |= 0x20)
+#define SET_LED_CLK()   ( AT91C_BASE_PIOA->PIO_SODR |= 0x40)//CLK
+#define CLR_LED_CLK()   ( AT91C_BASE_PIOA->PIO_CODR |= 0x40)
+
+
+#define PA0_SODR()      ( AT91C_BASE_PIOA->PIO_SODR |= 0x01)
+#define PA0_CODR()      ( AT91C_BASE_PIOA->PIO_CODR |= 0x01)
+#define PA1_SODR()      ( AT91C_BASE_PIOA->PIO_SODR |= 0x02)
+#define PA1_CODR()      ( AT91C_BASE_PIOA->PIO_CODR |= 0x02)
+#define PA2_SODR()      ( AT91C_BASE_PIOA->PIO_SODR |= 0x04)
+#define PA2_CODR()      ( AT91C_BASE_PIOA->PIO_CODR |= 0x04)
+
+#define SCAN_LINE_MODE                		(u8)8 
+
+#define P1_SIGN_DATA_LINES_OFFSET  0
+
+
+
+
+
+
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Protected functions                                                                                                */
 /*--------------------------------------------------------------------------------------------------------------------*/
